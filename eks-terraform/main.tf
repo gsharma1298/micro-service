eaa2842be86e8376c8750179552fd0d1abdac31f
gsharma1298 <gsharma1298@gmail.com>
2026-05-31 22:09:46 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -189,9 +189,9 @@ resource "aws_eks_node_group" "node-grp" {
   }
 
   scaling_config {
-    desired_size = 3
+    desired_size = 1
     max_size     = 10
-    min_size     = 2
+    min_size     = 1
   }
 
   update_config {
